9e8b118ffdda7e16384f11e45338f0b4b1a46a4e
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-21 21:14:04 +0300

    registered the models in the admin

--- myschool/admin.py
+++ myschool/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Teacher, Scores, Student, Fee
 
 # Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Scores)
+admin.site.register(Student)
+admin.site.register(Fee)
