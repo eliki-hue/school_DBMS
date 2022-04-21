7dd4c30df03dbd92f233b225d456fe12bc46fd42
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-21 13:47:01 +0300

    reinstalled requirements

--- myschool/models.py
+++ myschool/models.py
@@ -1,3 +1,44 @@
+from email.policy import default
 from django.db import models
+from django.forms import CharField, IntegerField
+from sqlalchemy import ForeignKey
 
 # Create your models here.
+class Teacher(models.Model):
+    name = CharField(max_length=50)
+    adm_no = IntegerField()
+    tsc_no =IntegerField()
+    subject1 = CharField(max_length=20)
+    subject2 = CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    f_name = CharField(max_length=30)
+    l_name = CharField(max_length=30)
+    form = CharField(max_length=5)
+    parent_name =CharField(max_length=50)
+    parent_phone = IntegerField()
+
+    def __str__(self):
+        return self.f_name
+
+class Fee(models.Model):
+    student= models.ForeignKey(Student, on_delete=models.CASCADE)
+    paid = IntegerField()
+    balance =IntegerField()
+
+
+class Scores(models.Model):
+    year = IntegerField()
+    term = IntegerField()
+    english = IntegerField()
+    kiswahili = IntegerField()
+    mathematics = IntegerField()
+    physics = IntegerField()
+    biology = IntegerField()
+    chemistry = IntegerField()
+
+    def __str__(self):
+        return self.year
--- school/settings.py
+++ school/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import bootstrap3
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myschool',
+    'cloudinary',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +80,12 @@ WSGI_APPLICATION = 'school.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'school',
+        'USER':'elijah',
+        'PASSWORD':'eliki13720'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
