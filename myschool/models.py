fdd772be397058e52f05be8666329e8c3fbc9638
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-04-21 21:25:21 +0300

    added missing 'model' in field names

--- myschool/models.py
+++ myschool/models.py
@@ -5,40 +5,40 @@ from django.forms import CharField, IntegerField
 
 # Create your models here.
 class Teacher(models.Model):
-    name = CharField(max_length=50)
-    adm_no = IntegerField()
-    tsc_no =IntegerField()
-    subject1 = CharField(max_length=20)
-    subject2 = CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    adm_no = models.IntegerField()
+    tsc_no =models.IntegerField()
+    subject1 = models.CharField(max_length=20)
+    subject2 = models.CharField(max_length=20)
 
     def __str__(self):
         return self.name
 
 class Student(models.Model):
-    f_name = CharField(max_length=30)
-    l_name = CharField(max_length=30)
-    form = CharField(max_length=5)
-    parent_name =CharField(max_length=50)
-    parent_phone = IntegerField()
+    f_name = models.CharField(max_length=30)
+    l_name = models.CharField(max_length=30)
+    form = models.CharField(max_length=5)
+    parent_name =models.CharField(max_length=50)
+    parent_phone = models.IntegerField()
 
     def __str__(self):
         return self.f_name
 
 class Fee(models.Model):
     student= models.ForeignKey(Student, on_delete=models.CASCADE)
-    paid = IntegerField()
-    balance =IntegerField()
+    paid = models.IntegerField()
+    balance =models.IntegerField()
 
 
 class Scores(models.Model):
-    year = IntegerField()
-    term = IntegerField()
-    english = IntegerField()
-    kiswahili = IntegerField()
-    mathematics = IntegerField()
-    physics = IntegerField()
-    biology = IntegerField()
-    chemistry = IntegerField()
+    year = models.IntegerField()
+    term = models.IntegerField()
+    english = models.IntegerField()
+    kiswahili = models.IntegerField()
+    mathematics = models.IntegerField()
+    physics = models.IntegerField()
+    biology = models.IntegerField()
+    chemistry = models.IntegerField()
 
     def __str__(self):
         return self.year
